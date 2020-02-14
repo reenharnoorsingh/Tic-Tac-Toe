fe4f3309c48f140d62c668f8a6a0ff5aeeb557e6
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2020-02-14 05:27:52 -0500

    Update game.c

--- C/game.c
+++ C/game.c
@@ -92,10 +92,10 @@ int IsValidBoard(int m, int n, char board[][n]) // checks the validity of the in
         }
     }
     if (x - o == -1 || x - o == 1 || x - o == 0)
-        printf("The board is valid\n ");
+        printf("The board is valid\n");
     else
     {
-        printf("the board is invalid\n");
+        printf("The board is invalid\n");
         printf("Start the game again!");
         exit(0); //exits the game since there is an Invalid board
     }
@@ -194,29 +194,63 @@ char WhoIsTheWinner(int m, int n, char board[][n]) //checks the inputs and if th
         exit(0);
     }
 }
+int PlayGame(int m, int n, char board[][n])
+{
+
+    printf("Welcome!\n");
+    printf("GameBoard!!\n");
+    InitializeBoard(3, 3, board);
+    PrintBoard(3, 3, board);
+    for (int i = 0; i < 9; i++) //loop needed to run the program to provide chances for the players
+    {
+        CreateBoard(3, 3, board);
+        IsValidBoard(3, 3, board);
+        ListWinningCells(3, 3, board);
+        WhoIsTheWinner(3, 3, board);
+    }
+}
 int main()
 {
-    int option;
     char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}}; // 3x3 grid with numbers 1-9
-    printf("Welcome!\n");
-    printf("MAIN MENU\n");
-    printf("Type 1 to play or 0 to exit ");
-    scanf("%d", &option);
-    if (option == 1)
+    char goption;
+    printf("press 'p' to print the tictactoe board\n");                           //only prints board
+    printf("press 'c' to create a tictactoe board with X and O cells\n");         //creates board user can input
+    printf("press 't' to test if a tictactoe board is valid or invalid board\n"); // only gives board validity
+    printf("press 'w' to predict the winning cell for player X or O\n");          // only gives hints to the user
+    printf("press 'g' to play the full game\n");                                  // full game with all features and users can determine winners
+    printf("press 'e' to exit\n");
+    scanf("%c", &goption);
+    switch (goption)
     {
-        InitializeBoard(3, 3, board);
+    case 'p':
         PrintBoard(3, 3, board);
-        for (int i = 0; i < 9; i++) //loop needed to run the program to provide chances for the players
-        {
+        break;
+    case 'c':
+        for (int i = 0; i < 9; i++)
             CreateBoard(3, 3, board);
+        break;
+    case 't':
+        for (int i = 0; i < 9; i++)
+        {
+            CreateBoard(3, 3, board); //generates game board for IsValidBoard()
             IsValidBoard(3, 3, board);
-            ListWinningCells(3, 3, board);
-            WhoIsTheWinner(3, 3, board);
         }
-    }
-    else
-    {
-        printf("Exit Game!  Thank You!");
+        break;
+    case 'w':
+        for (int i = 0; i < 9; i++)
+        {
+            CreateBoard(3, 3, board); //generates game board for ListWinningCells()
+            ListWinningCells(3, 3, board);
+        }
+        break;
+    case 'g':
+        PlayGame(3, 3, board);
+        break;
+    case 'e':
         exit(0);
+        break;
+    default:
+        printf("Try again!");
+        break;
     }
 }
